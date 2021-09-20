67eb6564417b70a24c3d8bff91cf0aa5767d9b3b
Guy ROUAULT <guy.rouault@l-3s.com>
2021-09-20 16:03:17 +0200

    pb pull

--- QtVersion.bat
+++ QtVersion.bat
@@ -1,6 +1,6 @@
 rem ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 rem Path for QT toolkit
 rem ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-set QTDIR=C:\Qt
-set QT_VERSION=5.14.0
-set PATH=%QTDIR%\%QT_VERSION%\msvc2017_64\bin;%QTDIR%\Tools\QtCreator\bin;%PATH%
+set QTDIR=D:\TOOLS\Qt
+set QT_VERSION=5.15.0
+set PATH=%QTDIR%\%QT_VERSION%\msvc2019_64\bin;%QTDIR%\Tools\QtCreator\bin;%PATH%
