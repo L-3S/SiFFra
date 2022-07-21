176566389c71500ca64849875ebe3d44ccd37723
Mathieu Vallee <mathieu.vallee@cea.fr>
2022-07-21 15:38:36 +0200

    Adding new tests (see README.md)

---
+++ Tests/testApi2/testapi.h
@@ -0,0 +1,20 @@
+#ifndef TESTAPI_H
+#define TESTAPI_H
+
+#include <iostream>
+#include "FbsfApi.h"
+
+using namespace std;
+//class TestFBSFAPI: public QObject
+//{
+//        Q_OBJECT
+//private slots:
+//    void LoadSuccess();
+//    void LoadFailure(int argc, char *argv[]);
+//    void SimpleSimu(int argc, char *argv[]);
+//    void WallClock(int argc, char *argv[]);
+//    void SaveRestore(int argc, char *argv[]);
+//    void RestoreWithCorr(int argc, char *argv[]);
+//    void TerminateAndReload(int argc, char *argv[]);
+
+#endif // TESTAPI_H
